b097653d6f2717600f62d1f123b7188cfb0ed38b
MeperDonas <santienrike16anime@gmail.com>
2025-03-25 18:14:13 -0500

    ruta de futuro historial

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -4,4 +4,9 @@ const homeController = require("../controllers/homeController");
 
 router.get("/", homeController.home);
 
+app.get("/api/historial", async (req, res) => {
+  const [rows] = await db.query("SELECT * FROM historial ORDER BY fecha DESC");
+  res.json(rows);
+});
+
 module.exports = router;
